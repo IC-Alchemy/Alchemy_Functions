4c24d93e4d90aba5ca31cf8587e1222d39c50759
IC-Alchemy <153705720+IC-Alchemy@users.noreply.github.com>
2024-04-06 23:23:07 -0600

    added increment and wrap count

--- functions.cpp
+++ functions.cpp
@@ -5,3 +5,12 @@ uint8_t widenHarmony(uint8_t& note1, uint8_t& note2) {
   }
   return note2;
 }
+
+ //  often used to process the clock counters for gate sequences
+int incrementAndWrapCount(u_int8_t& count, u_int8_t maxSize) {           
+  count++;
+  if (count > maxSize) {
+    count = 0;
+  }
+}
+
